f69943cb2092663ee60aab0086d89802b1d76099
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-12-20 13:50:21 +0700

    wip: remove init styles block in app entry

--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/AppEntry.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/AppEntry.kt
@@ -1,15 +1,10 @@
 package vn.id.tozydev.lucidabyss
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.css.ScrollBehavior
-import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
-import com.varabyte.kobweb.silk.init.InitSilk
-import com.varabyte.kobweb.silk.init.InitSilkContext
-import com.varabyte.kobweb.silk.init.registerStyleBase
 import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
 import com.varabyte.kobweb.silk.style.toModifier
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
@@ -17,24 +12,15 @@ import org.jetbrains.compose.web.css.*
 import vn.id.tozydev.lucidabyss.theme.ThemeMode
 import vn.id.tozydev.lucidabyss.theme.saveToLocalStorage
 
-@InitSilk
-fun initStyles(ctx: InitSilkContext) {
-    ctx.stylesheet.apply {
-        registerStyleBase("body") { Modifier.scrollBehavior(ScrollBehavior.Smooth) }
-    }
-}
-
+@Suppress("VariableNeverRead", "AssignedValueIsNeverRead")
 @App
 @Composable
 fun AppEntry(content: @Composable () -> Unit) {
     SilkApp {
         val themeMode by ThemeMode.currentState
-
-        @Suppress("VariableNeverRead")
         var colorMode by ColorMode.currentState
         LaunchedEffect(themeMode) {
             themeMode.saveToLocalStorage()
-            @Suppress("AssignedValueIsNeverRead")
             colorMode = themeMode.colorMode
         }
         Surface(

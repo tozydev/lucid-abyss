b5a0d0016da1bcc6aeca7e6c39a45ae7ee1cbdcd
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-01-03 20:55:52 +0700

    refactor: enhance TechStack component

--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/sections/TechStack.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/sections/TechStack.kt
@@ -1,23 +1,35 @@
 package vn.id.tozydev.lucidabyss.components.sections
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.css.BackgroundClip
 import com.varabyte.kobweb.compose.css.FontWeight
-import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.css.TextTransform
+import com.varabyte.kobweb.compose.css.TransitionProperty
+import com.varabyte.kobweb.compose.css.TransitionTimingFunction
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.icons.fa.FaJava
 import com.varabyte.kobweb.silk.components.icons.fa.FaLayerGroup
 import com.varabyte.kobweb.silk.components.text.SpanText
-import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.extendedBy
+import com.varabyte.kobweb.silk.style.selectors.hover
 import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.style.vars.animation.TransitionDurationVars
+import com.varabyte.kobweb.silk.theme.colors.ColorPalettes
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
+import vn.id.tozydev.lucidabyss.components.widgets.Badge
+import vn.id.tozydev.lucidabyss.components.widgets.BadgeVars
 import vn.id.tozydev.lucidabyss.components.widgets.ColumnIslandVariant
 import vn.id.tozydev.lucidabyss.components.widgets.Island
+import vn.id.tozydev.lucidabyss.components.widgets.NoneTransformBadgeVariant
+import vn.id.tozydev.lucidabyss.styles.ColorVars
+import vn.id.tozydev.lucidabyss.styles.TextSmStyle
+import vn.id.tozydev.lucidabyss.utils.kotlinGradient
 
 @Composable
 fun TechStack(modifier: Modifier = Modifier) {
@@ -26,9 +38,17 @@ fun TechStack(modifier: Modifier = Modifier) {
         variant = ColumnIslandVariant,
     ) {
         Row(
+            modifier = Modifier.margin(bottom = 1.cssRem),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            H2 {
+            H2(
+                TextSmStyle
+                    .toModifier()
+                    .color(ColorVars.TextLabel.value())
+                    .fontWeight(FontWeight.SemiBold)
+                    .textTransform(TextTransform.Uppercase)
+                    .toAttrs(),
+            ) {
                 FaLayerGroup(Modifier.margin(right = 0.5.cssRem))
                 Text("Công nghệ sử dụng")
             }
@@ -36,31 +56,53 @@ fun TechStack(modifier: Modifier = Modifier) {
         Row(
             modifier = Modifier.flexWrap(FlexWrap.Wrap).gap(1.cssRem),
         ) {
-            TechItem("Java", { FaJava() })
             TechItem(
-                "Kotlin",
-                {
-                    SpanText("K", Modifier.fontSize(1.5.cssRem).fontWeight(FontWeight.Bold))
+                label = "Java",
+                icon = { FaJava(Modifier.color(ColorPalettes.Orange._500)) },
+            )
+            TechItem(
+                label = "Kotlin",
+                icon = {
+                    SpanText(
+                        "K",
+                        Modifier
+                            .fontSize(1.5.cssRem)
+                            .fontWeight(FontWeight.Bold)
+                            .backgroundImage(kotlinGradient())
+                            .color(Colors.Transparent)
+                            .background { clip(BackgroundClip.Text) },
+                    )
                 },
             )
-            TechItem("Java", { FaJava() })
-            TechItem("Java", { FaJava() })
-            TechItem("Java", { FaJava() })
-            TechItem("Java", { FaJava() })
-            TechItem("Java", { FaJava() })
-            TechItem("Java", { FaJava() })
-            TechItem("Java", { FaJava() })
         }
     }
 }
 
-val TechItemStyle =
-    CssStyle.base {
-        Modifier
-            .borderRadius(0.5.cssRem)
-            .border(1.px, LineStyle.Solid, Colors.Gray) // todo: use theme color
-            .backgroundColor(Colors.LightGray) // todo: use theme color
-            .padding(topBottom = 0.5.cssRem, leftRight = 0.75.cssRem)
+val TechItemBadgeVariant =
+    NoneTransformBadgeVariant.extendedBy({ TextSmStyle.toModifier() }) {
+        base {
+            Modifier
+                .fontWeight(FontWeight.Medium)
+                .display(DisplayStyle.Flex)
+                .alignItems(AlignItems.Center)
+                .justifyContent(JustifyContent.Center)
+                .gap(0.5.cssRem)
+                .padding(topBottom = 0.5.cssRem, leftRight = 0.75.cssRem)
+                .setVariable(BadgeVars.Color, ColorVars.TextBody.value())
+                .setVariable(BadgeVars.BackgroundColor, Colors.Transparent)
+                .setVariable(BadgeVars.BorderColor, ColorVars.OutlineVariant.value())
+                .transition {
+                    property(TransitionProperty.All)
+                    duration(TransitionDurationVars.Fast.value())
+                    timingFunction(TransitionTimingFunction.cubicBezier(0.25, 0.8, 0.25, 1.0))
+                }
+        }
+        hover {
+            Modifier
+                .fontWeight(FontWeight.SemiBold)
+                .setVariable(BadgeVars.BackgroundColor, ColorVars.BgHighlight.value())
+                .setVariable(BadgeVars.BorderColor, ColorVars.Primary.value())
+        }
     }
 
 @Composable
@@ -69,11 +111,7 @@ fun TechItem(
     icon: @Composable () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
-        modifier = TechItemStyle.toModifier().then(modifier),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(0.5.cssRem),
-    ) {
+    Badge(modifier, TechItemBadgeVariant) {
         icon()
         Text(label)
     }

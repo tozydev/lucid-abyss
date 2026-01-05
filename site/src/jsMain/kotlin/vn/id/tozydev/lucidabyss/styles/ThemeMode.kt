973a1a7687821f8480fcdf9fc680cf8d7429b54b
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-01-05 10:14:49 +0700

    fix: prevent theme flicker on initial load

--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/styles/ThemeMode.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/styles/ThemeMode.kt
@@ -4,10 +4,14 @@ import androidx.compose.runtime.*
 import com.varabyte.kobweb.browser.storage.createStorageKey
 import com.varabyte.kobweb.browser.storage.getItem
 import com.varabyte.kobweb.browser.storage.setItem
+import com.varabyte.kobweb.core.AppGlobals
+import com.varabyte.kobweb.core.isExporting
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.varabyte.kobweb.silk.theme.colors.cssClass
 import com.varabyte.kobweb.silk.theme.colors.systemPreference
+import kotlinx.browser.document
 import kotlinx.browser.window
 import vn.id.tozydev.lucidabyss.styles.ThemeMode.System
 
@@ -58,4 +62,26 @@ fun ThemeMode.saveToLocalStorage() {
 @InitSilk
 fun initThemeMode(ctx: InitSilkContext) {
     ctx.config.initialColorMode = ThemeMode.loadFromLocalStorage().colorMode
+    if (!AppGlobals.isExporting) {
+        return
+    }
+
+    // Prevent flickering on the initial load because the main script is executed after HTML is rendered
+    document.head?.appendChild(
+        document.createElement("script").apply {
+            textContent =
+                """
+                const themeMode = localStorage.getItem("${storageKey.name}")
+                const colorClass = themeMode === ${ThemeMode.Light.name}
+                    ? ${ColorMode.LIGHT.cssClass}
+                    : (themeMode === ${ThemeMode.Dark.name}
+                        ? ${ColorMode.DARK.cssClass}
+                        : ${ColorMode.systemPreference.cssClass})
+                const oppositeColorClass = colorClass === ${ColorMode.LIGHT.cssClass}
+                    ? ${ColorMode.DARK.cssClass}
+                    : ${ColorMode.LIGHT.cssClass}
+                document.documentElement.classList.replace(oppositeColorClass, colorClass)
+                """.trimIndent()
+        },
+    )
 }

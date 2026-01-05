f340b0d7670f8ac88c2e89d3a4a939ba4828f37b
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-01-05 13:18:12 +0700

    refactor: improve blog page with styles and remove unneed now components

--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/pages/blog/Index.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/pages/blog/Index.kt
@@ -1,6 +1,7 @@
 package vn.id.tozydev.lucidabyss.pages.blog
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
@@ -9,21 +10,21 @@ import com.varabyte.kobweb.core.data.add
 import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
 import com.varabyte.kobweb.core.layout.Layout
+import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.addVariant
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
-import com.varabyte.kobweb.silk.style.cssRule
+import com.varabyte.kobweb.silk.style.toAttrs
 import com.varabyte.kobweb.silk.style.toModifier
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import vn.id.tozydev.lucidabyss.components.layouts.PageLayoutData
-import vn.id.tozydev.lucidabyss.components.widgets.BlogFilters
 import vn.id.tozydev.lucidabyss.components.widgets.ColumnIslandVariant
 import vn.id.tozydev.lucidabyss.components.widgets.Island
-import vn.id.tozydev.lucidabyss.components.widgets.Pagination
 import vn.id.tozydev.lucidabyss.components.widgets.PostCard
-import vn.id.tozydev.lucidabyss.components.widgets.PostCardStyle
 import vn.id.tozydev.lucidabyss.generated.filePathToPost
+import vn.id.tozydev.lucidabyss.styles.ColorVars
+import vn.id.tozydev.lucidabyss.styles.Text4XlStyle
+import vn.id.tozydev.lucidabyss.styles.TextLgStyle
 
 val BlogListStyle =
     CssStyle {
@@ -44,22 +45,6 @@ val BlogListStyle =
         }
     }
 
-val FeaturedPostCardVariant =
-    PostCardStyle.addVariant {
-        Breakpoint.MD {
-            Modifier
-                .gridColumn("span 2")
-                .display(DisplayStyle.Flex)
-                .flexDirection(FlexDirection.Row)
-        }
-
-        cssRule(Breakpoint.MD, "> :first-child") {
-            Modifier
-                .fillMaxHeight()
-                .width(50.percent)
-        }
-    }
-
 @InitRoute
 fun initBlogPage(ctx: InitRouteContext) {
     ctx.data.add(PageLayoutData("Blog"))
@@ -70,43 +55,37 @@ fun initBlogPage(ctx: InitRouteContext) {
 @Composable
 fun BlogPage() {
     Div {
-        PageHeader(Modifier.margin(bottom = 1.cssRem))
-        BlogFilters(Modifier.margin(bottom = 2.cssRem))
+        BlogHeader(Modifier.margin(bottom = 2.cssRem))
         Div(BlogListStyle.toModifier().toAttrs()) {
-            PostCard(
-                post = filePathToPost.values.first(),
-                variant = FeaturedPostCardVariant,
-            )
             filePathToPost.values.forEach {
                 PostCard(it)
             }
-        }
-
-        Div(
-            Modifier
-                .display(DisplayStyle.Flex)
-                .justifyContent(JustifyContent.Center)
-                .margin(top = 1.cssRem)
-                .toAttrs(),
-        ) {
-            Pagination()
         }
     }
 }
 
 @Composable
-private fun PageHeader(modifier: Modifier = Modifier) {
+private fun BlogHeader(modifier: Modifier = Modifier) {
     Island(
-        modifier = modifier,
+        modifier = Modifier.padding(3.cssRem) then modifier,
         variant = ColumnIslandVariant,
     ) {
-        H1 {
-            Text("Chia sẻ trải nghiệm")
-            Br()
-            Text("Lập trình và cuộc sống")
-        }
-        P {
-            Text("Nơi lưu lại những bài học, kinh nghiệm và những điều thú vị của tôi trong cuộc sống và công việc.")
+        Div(Modifier.maxWidth(42.cssRem).toAttrs()) {
+            H1(
+                Text4XlStyle
+                    .toModifier()
+                    .fontWeight(FontWeight.Bold)
+                    .color(ColorVars.TextHeading.value())
+                    .margin(bottom = 1.cssRem)
+                    .toAttrs(),
+            ) {
+                Text("Chia sẻ kiến thức")
+                Br()
+                SpanText("Lập trình và cuộc sống", Modifier.color(ColorVars.Primary.value()))
+            }
+            P(TextLgStyle.toAttrs()) {
+                Text("Nơi lưu lại những bài học, kinh nghiệm và những điều thú vị trong hành trình phát triển bản thân để trở nên tốt hơn.")
+            }
         }
     }
 }

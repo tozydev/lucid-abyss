152e2ca7aa16c32802774b32cf2c9a56e6e4edb3
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-01-05 15:53:42 +0700

    refactor: update Dicussions styles

--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/sections/Discussion.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/sections/Discussion.kt
@@ -1,16 +1,19 @@
 package vn.id.tozydev.lucidabyss.components.sections
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.icons.fa.FaComments
 import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.style.toModifier
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import vn.id.tozydev.lucidabyss.components.widgets.ColumnIslandVariant
 import vn.id.tozydev.lucidabyss.components.widgets.Island
+import vn.id.tozydev.lucidabyss.styles.ColorVars
+import vn.id.tozydev.lucidabyss.styles.TextXlStyle
 
 @Composable
 fun Discussion(modifier: Modifier = Modifier) {
@@ -18,7 +21,14 @@ fun Discussion(modifier: Modifier = Modifier) {
         modifier = modifier,
         variant = ColumnIslandVariant,
     ) {
-        H3 {
+        H3(
+            TextXlStyle
+                .toModifier()
+                .fontWeight(FontWeight.SemiBold)
+                .margin(bottom = 1.cssRem)
+                .color(ColorVars.TextHeading.value())
+                .toAttrs(),
+        ) {
             Text("Thảo luận")
         }
         Div(
@@ -28,11 +38,12 @@ fun Discussion(modifier: Modifier = Modifier) {
                 .alignItems(AlignItems.Center)
                 .justifyContent(JustifyContent.Center)
                 .borderRadius(0.5.cssRem)
-                .backgroundColor(Colors.LightGray)
+                .gap(0.5.cssRem)
+                .backgroundColor(ColorVars.BgContainer.value())
                 .padding(2.cssRem)
                 .toAttrs(),
         ) {
-            FaComments()
+            FaComments(TextXlStyle.toModifier())
             SpanText("Chức năng thảo luận sẽ sớm được ra mắt!")
         }
     }

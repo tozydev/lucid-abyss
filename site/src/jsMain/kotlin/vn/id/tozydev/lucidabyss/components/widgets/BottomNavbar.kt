92f39c7a7bd0a03f98292824fba6369f820aa5f6
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-01-14 14:18:50 +0700

    refactor: change nav component to dock in botton nav

--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/widgets/BottomNavbar.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/widgets/BottomNavbar.kt
@@ -1,18 +1,14 @@
 package vn.id.tozydev.lucidabyss.components.widgets
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.foundation.layout.Arrangement
-import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.rememberPageContext
-import com.varabyte.kobweb.navigation.Anchor
 import com.varabyte.kobweb.silk.components.icons.fa.FaCubes
 import com.varabyte.kobweb.silk.components.icons.fa.FaHouse
 import com.varabyte.kobweb.silk.components.icons.fa.FaRss
 import com.varabyte.kobweb.silk.components.icons.fa.FaUser
-import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import vn.id.tozydev.lucidabyss.utils.tw
 
@@ -21,7 +17,7 @@ fun BottomNavbar(modifier: Modifier = Modifier) {
     val ctx = rememberPageContext()
 
     @Composable
-    fun MenuItem(
+    fun DockItem(
         path: String,
         label: String,
         icon: @Composable () -> Unit,
@@ -33,49 +29,42 @@ fun BottomNavbar(modifier: Modifier = Modifier) {
                 ctx.route.path.startsWith(path)
             }
 
-        Li(
+        Button(
             Modifier
-                .tw("flex-1 items-center")
-                .toAttrs {
+                .onClick {
+                    ctx.router.navigateTo(path)
+                }.toAttrs {
                     if (isActive) {
-                        tw("menu-active")
+                        tw("dock-active")
                     }
                 },
         ) {
-            Anchor(
-                href = path,
-                attrs = { tw("size-full justify-center") },
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(0.125.cssRem),
-                ) {
-                    icon()
-                    Text(label)
-                }
+            Span({ tw("size-[1.2em]") }) {
+                icon()
+            }
+            Span({ tw("dock-label") }) {
+                Text(label)
             }
         }
     }
 
-    Nav(
+    Div(
         Modifier
-            .tw("navbar bg-base-100 fixed z-50 left-0 right-0 bottom-0 mx-auto min-h-auto p-0 md:hidden")
+            .tw("dock dock-xs md:hidden")
             .then(modifier)
             .toAttrs(),
     ) {
-        Ul({ tw("menu menu-sm menu-horizontal gap-1 w-full font-medium pb-0") }) {
-            MenuItem("/", "Trang chủ") {
-                FaHouse()
-            }
-            MenuItem("/me", "Về tôi") {
-                FaUser()
-            }
-            MenuItem("/blog/", "Blog") {
-                FaRss()
-            }
-            MenuItem("/san-pham", "Sản phẩm") {
-                FaCubes()
-            }
+        DockItem("/", "Trang chủ") {
+            FaHouse()
+        }
+        DockItem("/me", "Về tôi") {
+            FaUser()
+        }
+        DockItem("/blog/", "Blog") {
+            FaRss()
+        }
+        DockItem("/san-pham", "Sản phẩm") {
+            FaCubes()
         }
     }
 }

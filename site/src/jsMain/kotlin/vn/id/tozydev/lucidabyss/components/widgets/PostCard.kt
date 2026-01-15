2218fe2a9e2ff33873e7ef541b9da7827013293a
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-01-15 13:27:35 +0700

    feat: redesign blog card and blog layout

--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/widgets/PostCard.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/widgets/PostCard.kt
@@ -1,153 +1,47 @@
 package vn.id.tozydev.lucidabyss.components.widgets
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.css.Cursor
-import com.varabyte.kobweb.compose.css.FontWeight
-import com.varabyte.kobweb.compose.css.ObjectFit
-import com.varabyte.kobweb.compose.css.Overflow
-import com.varabyte.kobweb.compose.css.PointerEvents
 import com.varabyte.kobweb.compose.dom.GenericTag
-import com.varabyte.kobweb.compose.foundation.layout.Arrangement
-import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.foundation.layout.Row
-import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
-import com.varabyte.kobweb.silk.components.graphics.Image
-import com.varabyte.kobweb.silk.components.graphics.ImageDecoding
-import com.varabyte.kobweb.silk.components.graphics.ImageLoading
 import com.varabyte.kobweb.silk.components.icons.fa.FaCalendar
-import com.varabyte.kobweb.silk.components.navigation.Link
-import com.varabyte.kobweb.silk.components.navigation.UndecoratedLinkVariant
-import com.varabyte.kobweb.silk.components.text.SpanText
-import com.varabyte.kobweb.silk.style.ComponentKind
-import com.varabyte.kobweb.silk.style.CssLayer
-import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.CssStyleVariant
-import com.varabyte.kobweb.silk.style.base
-import com.varabyte.kobweb.silk.style.extendedBy
-import com.varabyte.kobweb.silk.style.selectors.hover
-import com.varabyte.kobweb.silk.style.toAttrs
-import com.varabyte.kobweb.silk.style.toModifier
-import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import vn.id.tozydev.lucidabyss.models.Post
 import vn.id.tozydev.lucidabyss.models.coverImagePathOrDefault
-import vn.id.tozydev.lucidabyss.styles.ColorVars
-import vn.id.tozydev.lucidabyss.styles.TextSmStyle
-import vn.id.tozydev.lucidabyss.styles.TextXlStyle
-import vn.id.tozydev.lucidabyss.styles.TextXsStyle
 import vn.id.tozydev.lucidabyss.utils.formatDate
-
-sealed interface PostCardKind : ComponentKind
-
-val PostCardStyle =
-    CssStyle<PostCardKind>(
-        {
-            IslandStyle.toModifier(
-                ColumnIslandVariant,
-                NoPaddingIslandVariant,
-                SoftLiftingIslandVariant,
-            )
-        },
-    ) {
-        base {
-            Modifier.fillMaxSize()
-        }
-        hover {
-            Modifier
-                .pointerEvents(PointerEvents.Auto)
-                .cursor(Cursor.Pointer)
-        }
-    }
-
-val PostCardLinkVariant =
-    UndecoratedLinkVariant.extendedBy {
-        base {
-            Modifier.color(ColorVars.TextBody.value())
-        }
-    }
-
-@CssLayer("component-styles")
-val PostCardCoverContainerStyle =
-    CssStyle.base {
-        Modifier
-            .flexShrink(0)
-            .height(12.cssRem)
-            .position(Position.Relative)
-            .overflow(Overflow.Hidden)
-    }
+import vn.id.tozydev.lucidabyss.utils.tw
 
 @Composable
 fun PostCard(
     post: Post,
     modifier: Modifier = Modifier,
-    variant: CssStyleVariant<PostCardKind>? = null,
 ) {
-    Link(
-        path = post.route,
-        modifier = PostCardStyle.toModifier(variant).then(modifier),
-        variant = PostCardLinkVariant,
-    ) {
-        Div(PostCardCoverContainerStyle.toAttrs()) {
-            Image(
-                post.coverImagePathOrDefault,
-                alt = "Cover image for ${post.title}",
-                loading = ImageLoading.Lazy,
-                decoding = ImageDecoding.Auto,
-                modifier =
-                    Modifier
-                        .fillMaxSize()
-                        .position(Position.Absolute)
-                        .objectFit(ObjectFit.Cover)
-                        .top(0.px)
-                        .left(0.px)
-                        .right(0.px)
-                        .bottom(0.px),
-            )
-        }
-        Column(
+    A(
+        href = post.route,
+        attrs =
             Modifier
-                .padding(top = 1.cssRem, leftRight = 1.5.cssRem, bottom = 1.5.cssRem)
-                .gap(0.5.cssRem)
-                .fillMaxSize(),
-        ) {
-            Row(
-                modifier =
-                    TextSmStyle
-                        .toModifier()
-                        .color(ColorVars.TextLabel.value())
-                        .fillMaxWidth()
-                        .margin(bottom = 0.5.cssRem),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(0.5.cssRem),
-            ) {
-                Badge(TextXsStyle.toModifier(), variant = NoneTransformBadgeVariant) {
+                .tw("card card-border card-lg bg-base-100")
+                .then(modifier)
+                .toAttrs(),
+    ) {
+        GenericTag("figure") {
+            Img(
+                src = post.coverImagePathOrDefault,
+                alt = "Cover image for ${post.title}",
+            )
+            Div({ tw("absolute top-4 right-4") }) {
+                Div({ tw("badge") }) {
                     Text(post.topic)
                 }
-                SpanText("•")
-                GenericTag("time", attrsStr = "datetime=\"${post.publishedAt}\"") {
-                    FaCalendar()
-                    Text(" ${post.publishedAt.formatDate()}")
-                }
             }
-            H3(
-                TextXlStyle
-                    .toModifier()
-                    .fontWeight(FontWeight.SemiBold)
-                    .color(ColorVars.TextHeading.value())
-                    .toAttrs(),
-            ) {
-                Text(post.title)
+        }
+        Div({ tw("card-body") }) {
+            GenericTag("time", attrsStr = "datetime=\"${post.publishedAt}\"") {
+                FaCalendar(Modifier.tw("mr-1"))
+                Text(post.publishedAt.formatDate())
             }
-            P(Modifier.flex(1).toAttrs()) {
-                Text(post.description)
-            }
-
-            Div(TextSmStyle.toModifier().color(ColorVars.TextLabel.value()).toAttrs()) {
-                Text("Đọc thêm")
-            }
+            H3({ tw("card-title") }) { Text(post.title) }
+            P { Text(post.description) }
         }
     }
 }
--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/pages/blog/Index.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/pages/blog/Index.kt
@@ -1,49 +1,16 @@
 package vn.id.tozydev.lucidabyss.pages.blog
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.css.FontWeight
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.data.add
 import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
 import com.varabyte.kobweb.core.layout.Layout
-import com.varabyte.kobweb.silk.components.text.SpanText
-import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
-import com.varabyte.kobweb.silk.style.toAttrs
-import com.varabyte.kobweb.silk.style.toModifier
-import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import vn.id.tozydev.lucidabyss.components.layouts.PageLayoutData
-import vn.id.tozydev.lucidabyss.components.widgets.ColumnIslandVariant
-import vn.id.tozydev.lucidabyss.components.widgets.Island
 import vn.id.tozydev.lucidabyss.components.widgets.PostCard
 import vn.id.tozydev.lucidabyss.generated.filePathToPost
-import vn.id.tozydev.lucidabyss.styles.ColorVars
-import vn.id.tozydev.lucidabyss.styles.Text4XlStyle
-import vn.id.tozydev.lucidabyss.styles.TextLgStyle
-
-val BlogListStyle =
-    CssStyle {
-        base {
-            Modifier
-                .fillMaxWidth()
-                .display(DisplayStyle.Grid)
-                .gridTemplateColumns { repeat(1) { minmax(0.px, 1.fr) } }
-                .gap(1.5.cssRem)
-        }
-        Breakpoint.MD {
-            Modifier
-                .gridTemplateColumns { repeat(2) { minmax(0.px, 1.fr) } }
-        }
-        Breakpoint.LG {
-            Modifier
-                .gridTemplateColumns { repeat(3) { minmax(0.px, 1.fr) } }
-        }
-    }
+import vn.id.tozydev.lucidabyss.utils.tw
 
 @InitRoute
 fun initBlogPage(ctx: InitRouteContext) {
@@ -54,36 +21,28 @@ fun initBlogPage(ctx: InitRouteContext) {
 @Layout(".components.layouts.PageLayout")
 @Composable
 fun BlogPage() {
-    /* Div {
-        BlogHeader(Modifier.margin(bottom = 2.cssRem))
-        Div(BlogListStyle.toModifier().toAttrs()) {
+    Div({ tw("flex flex-col gap-8") }) {
+        BlogHeader()
+        Div({ tw("grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3") }) {
             filePathToPost.values.forEach {
                 PostCard(it)
             }
         }
-    } */
+    }
 }
 
 @Composable
-private fun BlogHeader(modifier: Modifier = Modifier) {
-    Island(
-        modifier = Modifier.padding(3.cssRem) then modifier,
-        variant = ColumnIslandVariant,
-    ) {
-        Div(Modifier.maxWidth(42.cssRem).toAttrs()) {
-            H1(
-                Text4XlStyle
-                    .toModifier()
-                    .fontWeight(FontWeight.Bold)
-                    .color(ColorVars.TextHeading.value())
-                    .margin(bottom = 1.cssRem)
-                    .toAttrs(),
-            ) {
+private fun BlogHeader() {
+    Div({ tw("card card-xl card-border bg-base-100") }) {
+        Div({ tw("card-body gap-4") }) {
+            H1({ tw("text-3xl md:text-4xl font-bold") }) {
                 Text("Chia sẻ kiến thức")
                 Br()
-                SpanText("Lập trình và cuộc sống", Modifier.color(ColorVars.Primary.value()))
+                Span({ tw("text-primary") }) {
+                    Text("Lập trình và cuộc sống")
+                }
             }
-            P(TextLgStyle.toAttrs()) {
+            P {
                 Text("Nơi lưu lại những bài học, kinh nghiệm và những điều thú vị trong hành trình phát triển bản thân để trở nên tốt hơn.")
             }
         }

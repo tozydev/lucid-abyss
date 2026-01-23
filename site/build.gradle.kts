10f424c76a02e5c80a118ea93e511dddf1102f57
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-01-23 21:32:53 +0700

    chore: remove unused kobweb-silk dependencies

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -47,7 +47,6 @@ kotlin {
             implementation(libs.compose.runtime)
             implementation(libs.compose.html.core)
             implementation(libs.kobweb.core)
-            implementation(libs.kobweb.silk)
             implementation(libs.silk.icons.fa)
             implementation(libs.kobwebx.markdown)
             implementation(libs.kotlinx.datetime)

ecafcae9495a6cdc241dc7ed9adb97b9660cb9f1
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-01-28 18:41:25 +0700

    build: add ksp plugn to class path

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kobweb.application) apply false
     alias(libs.plugins.kobwebx.markdown) apply false
     alias(libs.plugins.libres) apply false

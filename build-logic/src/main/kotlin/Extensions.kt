d05db404354a708c63735b595263dd1fa4373771
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-02-04 18:14:19 +0700

    refactor: move extra routes configuration to Extensions.kt

--- build-logic/src/main/kotlin/Extensions.kt
+++ build-logic/src/main/kotlin/Extensions.kt
@@ -1,7 +1,10 @@
+import com.varabyte.kobweb.gradle.application.extensions.AppBlock
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.provider.Provider
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
+import vn.id.tozydev.lucidabyss.core.SiteLanguage
+import vn.id.tozydev.lucidabyss.core.SitePaths
 
 fun KotlinDependencyHandler.npm(dependency: Provider<MinimalExternalModuleDependency>): Provider<NpmDependency> =
     dependency.map {
@@ -12,3 +15,14 @@ fun KotlinDependencyHandler.npm(dependency: Provider<MinimalExternalModuleDepend
             scope = NpmDependency.Scope.valueOf(it.group!!.uppercase()),
         )
     }
+
+internal fun AppBlock.ExportBlock.configureRoutes() {
+    SiteLanguage.entries.forEach { language ->
+        context(language) {
+            addExtraRoute(SitePaths.home)
+            addExtraRoute(SitePaths.about)
+            addExtraRoute(SitePaths.blog)
+            addExtraRoute(SitePaths.projects)
+        }
+    }
+}
--- build-logic/src/main/kotlin/site-export.lucid-abyss.gradle.kts
+++ build-logic/src/main/kotlin/site-export.lucid-abyss.gradle.kts
@@ -2,21 +2,11 @@ import com.varabyte.kobweb.gradle.application.KobwebApplicationPlugin
 import com.varabyte.kobweb.gradle.application.extensions.app
 import com.varabyte.kobweb.gradle.application.extensions.export
 import com.varabyte.kobweb.gradle.core.extensions.kobwebBlock
-import vn.id.tozydev.lucidabyss.core.SiteLanguage
-import vn.id.tozydev.lucidabyss.core.SitePaths
 
 plugins.withType<KobwebApplicationPlugin> {
     kobwebBlock.app.export.apply {
         enableTraces()
-
+        configureRoutes()
         suppressNoRootWarning = true
-        SiteLanguage.entries.forEach { language ->
-            context(language) {
-                addExtraRoute(SitePaths.home)
-                addExtraRoute(SitePaths.about)
-                addExtraRoute(SitePaths.blog)
-                addExtraRoute(SitePaths.projects)
-            }
-        }
     }
 }

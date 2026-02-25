20a58fd670504c6e7e71bc970ce224d6eee19bb0
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-02-25 07:49:40 +0700

    feat: move language state to SiteLanguage

--- core/src/commonMain/kotlin/vn/id/tozydev/lucidabyss/core/SiteLanguage.kt
+++ core/src/commonMain/kotlin/vn/id/tozydev/lucidabyss/core/SiteLanguage.kt
@@ -1,5 +1,9 @@
 package vn.id.tozydev.lucidabyss.core
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 enum class SiteLanguage(
     val code: String,
     val label: String,
@@ -10,6 +14,12 @@ enum class SiteLanguage(
 
     companion object {
         val Default = Vi
+
+        private val _currentState = MutableStateFlow(Default)
+        val currentState: StateFlow<SiteLanguage>
+            get() = _currentState.asStateFlow()
+
+        var current: SiteLanguage by _currentState::value
 
         fun fromCode(code: String) = entries.firstOrNull { it.code == code } ?: Default
     }

bb83982382ade09d4f16df58ccdb7e8d91ded3fa
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-03-29 18:40:00 +0700

    build: enhance blog processing task with attachments processing
    - Replaced `ProcessBlogContentTask` with `ProcessBlogPostsTask` for clarity and functionality expansion.
    - Added support for attachment processing with custom prefixes and structured output directories.
    - Streamlined frontmatter handling and PascalCase slug transformations for markdown files.
    - Integrated incremental processing for markdown posts and attachments.

--- build-logic/src/main/kotlin/blog.lucid-abyss.gradle.kts
+++ build-logic/src/main/kotlin/blog.lucid-abyss.gradle.kts
@@ -1,26 +1,40 @@
 import com.varabyte.kobweb.gradle.application.KobwebApplicationPlugin
 import com.varabyte.kobweb.gradle.core.extensions.kobwebBlock
+import com.varabyte.kobweb.gradle.core.kmp.buildTargets
+import com.varabyte.kobweb.gradle.core.kmp.kotlin
 import com.varabyte.kobwebx.gradle.markdown.KobwebxMarkdownPlugin
 import com.varabyte.kobwebx.gradle.markdown.MarkdownBlock
 import com.varabyte.kobwebx.gradle.markdown.tasks.ProcessMarkdownTask
-import vn.id.tozydev.lucidabyss.build.blog.ProcessBlogContentTask
+import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrTarget
+import vn.id.tozydev.lucidabyss.build.blog.ProcessBlogPostsTask
 import vn.id.tozydev.lucidabyss.build.blog.processBlogMarkdowns
 
 plugins.withType<KobwebApplicationPlugin> {
     apply<KobwebxMarkdownPlugin>()
 
-    val processBlogContent by tasks.registering(ProcessBlogContentTask::class) {
-        blogContentDir = rootProject.layout.projectDirectory.dir("blog")
-        processedBlogContentDir = layout.buildDirectory.dir("generated/$name/src/jsMain/resources/markdown")
+    val processBlogPosts by tasks.registering(ProcessBlogPostsTask::class) {
+        postsDir = rootProject.layout.projectDirectory.dir("blog")
+        attachmentsDirName = ".attachments"
+        attachmentPathPrefix = "/attachments"
+        processedPostsDir = layout.buildDirectory.dir("generated/$name/src/jsMain/markdown")
+        processedAttachmentsDir =
+            layout.buildDirectory.dir("generated/$name/src/jsMain/resources")
     }
 
     tasks.withType<ProcessMarkdownTask> {
-        dependsOn(processBlogContent)
+        dependsOn(processBlogPosts)
     }
 
     kobwebBlock.extensions.configure<MarkdownBlock> {
         process = { entries -> processBlogMarkdowns(entries) }
 
-        addSource(processBlogContent.flatMap { it.processedBlogContentDir }, ".pages.blog")
+        addSource(processBlogPosts.flatMap { it.processedPostsDir }, ".pages.blog")
+    }
+    kobwebBlock
+
+    buildTargets.withType<KotlinJsIrTarget> {
+        kotlin.sourceSets.named("${name}Main") {
+            resources.srcDirs(processBlogPosts.map { it.processedAttachmentsDir })
+        }
     }
 }

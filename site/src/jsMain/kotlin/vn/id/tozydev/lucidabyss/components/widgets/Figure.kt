8e5cd25fbc92172aa2ee6746b68dd8e34fc8439c
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-04-03 14:45:03 +0700

    refactor: update `Figure` attributes to use `HTMLElement` for broader compatibility

--- site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/widgets/Figure.kt
+++ site/src/jsMain/kotlin/vn/id/tozydev/lucidabyss/components/widgets/Figure.kt
@@ -3,14 +3,14 @@ package vn.id.tozydev.lucidabyss.components.widgets
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.dom.GenericTag
 import org.jetbrains.compose.web.dom.*
-import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLElement
 
 private const val FIGURE_TAG = "figure"
 
 @Composable
 fun Figure(
-    attrs: AttrBuilderContext<HTMLDivElement>? = null,
-    content: (@Composable ElementScope<HTMLDivElement>.() -> Unit)? = null,
+    attrs: AttrBuilderContext<HTMLElement>? = null,
+    content: (@Composable ElementScope<HTMLElement>.() -> Unit)? = null,
 ) {
     GenericTag(name = FIGURE_TAG, attrs = attrs, content = content)
 }
